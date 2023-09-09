f7f3ef054e5c4817f014ccf5039fc8e63f8ecd9a
CoffeeKiller <18320210361@163.com>
2023-09-09 14:20:31 +0800

    update 2023年9月9日14:20:30

--- Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/common/api/CommonResult.java
+++ Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/common/api/CommonResult.java
@@ -18,7 +18,54 @@ public class CommonResult<T> {
      * @param data 获取的数据
      * */
     public static <T> CommonResult<T> success(T data){
-        return new CommonResult<T>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(),data);
+        return new CommonResult<>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(), data);
     }
 
+    /**
+     * 响应错误, 并返回错误代码
+     * @param errorCode 错误码
+     * */
+    public static <T> CommonResult<T> failed(IErrorCode errorCode){
+        return new CommonResult<>(errorCode.getCode(), errorCode.getMessage(), null);
+    }
+
+    /**
+     * 响应错误, 并返回提示信息
+     * @param message 提示信息
+     * */
+    public static <T> CommonResult<T> failed(String message){
+        return new CommonResult<>(ResultCode.FAILED.getCode(), message, null);
+    }
+
+    /**
+     * 参数校验失败, 响应结果
+     * @param message 提示信息
+     * */
+    public static <T> CommonResult<T> validateFailed(String message){
+        return new CommonResult<>(ResultCode.VALIDATE_FAILED.getCode(), message, null);
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
