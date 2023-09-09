418daefce650ed653f8554f0c6220a44a6495311
CoffeeKiller <18320210361@163.com>
2023-09-09 13:15:12 +0800

    update 2023年9月9日13:15:09

--- Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/mbg/CommentGenerator.java
+++ Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/mbg/CommentGenerator.java
@@ -1,5 +1,8 @@
 package com.coffee.mall_tiny01.mbg;
 
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
 import org.mybatis.generator.internal.util.StringUtility;
 
@@ -17,4 +20,32 @@ public class CommentGenerator extends DefaultCommentGenerator {
         super.addConfigurationProperties(properties);
         this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
     }
+
+    /*
+    * 为字段添加注释
+    * */
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        String remarks = introspectedColumn.getRemarks();
+
+        // 根据参数和备注信息判断是否需要添加注释信息
+        if (addRemarkComments && StringUtility.stringHasValue(remarks)){
+            addFieldJavaDoc(field, remarks);
+        }
+    }
+
+    /*
+    * 给model的字段添加注释
+    * */
+    void addFieldJavaDoc(Field field, String remarks){
+        field.addJavaDocLine("/**"); // 文档注释开始
+
+        // 获取数据库字段的注释信息
+        String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+        for (String remarkLine : remarkLines) {
+            field.addJavaDocLine(" * " + remarkLine);
+        }
+        field.addJavaDocLine("*/"); // 文档注释结束
+    }
+
 }
