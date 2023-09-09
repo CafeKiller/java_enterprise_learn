371c63cbd0a775701b6d5a38f4e2d495049c089c
CoffeeKiller <18320210361@163.com>
2023-09-09 15:51:15 +0800

    update Controller and CommonPage

--- Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/controller/PmsBrandController.java
+++ Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/controller/PmsBrandController.java
@@ -1,6 +1,7 @@
 package com.coffee.mall_tiny01.controller;
 
 
+import com.coffee.mall_tiny01.common.api.CommonPage;
 import com.coffee.mall_tiny01.common.api.CommonResult;
 import com.coffee.mall_tiny01.mbg.model.PmsBrand;
 import com.coffee.mall_tiny01.service.PmsBrandService;
@@ -55,12 +56,13 @@ public class PmsBrandController {
 
     /**
      * 更新一个品牌信息
+     * @param id 品牌id
+     * @param pmsBrandDto 品牌更新信息
      * */
     @PostMapping("/update/{id}")
     @ResponseBody
     public CommonResult updateBrand(@PathVariable("id") Long id, @RequestBody PmsBrand pmsBrandDto, BindingResult result){
         CommonResult commonResult;
-
         int count = brandService.updateBrand(id, pmsBrandDto);
         if (count == 1){
             commonResult = CommonResult.success(pmsBrandDto);
@@ -72,8 +74,34 @@ public class PmsBrandController {
         return commonResult;
     }
 
-    /*
-    *
-    * */
+    /**
+     * 删除一个品牌信息
+     * @param id 品牌id
+     * */
+    @PostMapping("/delete/{id}")
+    @ResponseBody
+    public CommonResult deleteBrand(@PathVariable("id") long id){
+        int result = brandService.deleteBrand(id);
+        if (result == 1){
+            LOGGER.debug("deleteBrand success : id={}",id);
+            return CommonResult.success(null);
+        }else {
+            LOGGER.debug("deleteBrand failed : id={}",id);
+            return CommonResult.failed("删除失败");
+        }
+    }
+
+    /**
+     * 分页获取品牌信息
+     * @param pageNum 请求元素的起始位置 默认从1开始
+     * @param pageSize 请求的单页元素数量 默认单页取3位元素
+     * */
+    @GetMapping("/list")
+    @ResponseBody
+    public CommonResult<CommonPage<PmsBrand>> listBrand(@RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum,
+                                                        @RequestParam(value = "pageSize", defaultValue = "3") Integer pageSize){
+        List<PmsBrand> brandList = brandService.listBrand(pageNum, pageSize);
+        return CommonResult.success(CommonPage.restPage(brandList));
+    }
 
 }
