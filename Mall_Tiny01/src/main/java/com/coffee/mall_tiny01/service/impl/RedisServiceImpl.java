c33e0799a53f9b86edf0b0adef5419ab9219d233
CoffeeKiller <18320210361@163.com>
2023-09-24 22:10:22 +0800

    update 2023年9月24日22:10:21

--- Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/service/impl/RedisServiceImpl.java
+++ Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/service/impl/RedisServiceImpl.java
@@ -4,6 +4,8 @@ import com.coffee.mall_tiny01.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 public class RedisServiceImpl implements RedisService {
 
     @Autowired
@@ -11,26 +13,26 @@ public class RedisServiceImpl implements RedisService {
 
     @Override
     public void set(String key, String value) {
-
+        stringRedisTemplate.opsForValue().set(key, value);
     }
 
     @Override
     public String get(String key) {
-        return null;
+        return stringRedisTemplate.opsForValue().get(key);
     }
 
     @Override
     public boolean expire(String key, long expire) {
-        return false;
+        return stringRedisTemplate.expire(key, expire, TimeUnit.SECONDS);
     }
 
     @Override
     public void remove(String key) {
-
+        stringRedisTemplate.delete(key);
     }
 
     @Override
     public Long increment(String key, long delta) {
-        return null;
+        return stringRedisTemplate.opsForValue().increment(key, delta);
     }
 }
