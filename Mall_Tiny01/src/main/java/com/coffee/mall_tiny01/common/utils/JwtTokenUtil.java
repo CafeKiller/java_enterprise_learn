5388cae299b9a44c967cad42a25e40a1f0f9d0e7
CoffeeKiller <18320210361@163.com>
2023-09-25 22:09:41 +0800

    update 2023年9月25日22:09:40

--- Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/common/utils/JwtTokenUtil.java
+++ Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/common/utils/JwtTokenUtil.java
@@ -1,9 +1,14 @@
 package com.coffee.mall_tiny01.common.utils;
 
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
+import java.util.Map;
 
 /**
  * JwtToken生成的工具类
@@ -19,6 +24,22 @@ public class JwtTokenUtil {
     private Long expiration;
 
 
+    /**
+     * 根据负责生成的JWT的token
+     * */
+    private String generateToken(Map<String, Object> claims){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(generateExpirationDate())
+                .signWith(SignatureAlgorithm.ES512, secret)
+                .compact();
+    }
 
+    /**
+     * 生成token的过期时间
+     * */
+    private Date generateExpirationDate(){
+        return new Date(System.currentTimeMillis() + expiration * 1000);
+    }
 
 }
