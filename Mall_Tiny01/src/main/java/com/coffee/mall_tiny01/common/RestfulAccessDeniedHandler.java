0a05a860221450208e1de5b009b24f6878552538
CoffeeKiller <18320210361@163.com>
2023-09-26 23:48:20 +0800

    update 2023-9-26 23:48:19

--- Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/common/RestfulAccessDeniedHandler.java
+++ Mall_Tiny01/src/main/java/com/coffee/mall_tiny01/common/RestfulAccessDeniedHandler.java
@@ -3,6 +3,8 @@ package com.coffee.mall_tiny01.common;
 import cn.hutool.json.JSONUtil;
 import com.coffee.mall_tiny01.common.api.CommonResult;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import javax.servlet.ServletException;
@@ -10,15 +12,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class RestfulAccessDeniedHandler implements AccessDeniedHandler {
-    @Override
-    public void handle(HttpServletRequest httpServletRequest,
-                       HttpServletResponse httpServletResponse,
-                       AccessDeniedException e) throws IOException, ServletException {
+import static org.apache.coyote.http11.Constants.a;
 
+public class RestfulAccessDeniedHandler implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException authException) throws IOException, ServletException {
         httpServletResponse.setCharacterEncoding("UTF-8");
         httpServletResponse.setContentType("application/json");
-//        httpServletResponse.getWriter().println(JSONUtil.parse(CommonResult));
-
+        httpServletResponse.getWriter().println(JSONUtil.parse(CommonResult.unauthorized(authException.getMessage())));
+        httpServletResponse.getWriter().flush();
     }
 }
